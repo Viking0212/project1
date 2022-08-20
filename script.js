9fc4a84d148b0bfccf1d76111811cb931342be84
Elisa Nesterets <elisanesterets@MacBook-Air-Elisa.local>
2022-08-20 22:59:47 +0300

    init

---
+++ script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+let Number = 5;
+const leftborderwidth = 1;
+
+Number = 10;
+console.log(Number);
+
+const obj = {
+ a: 50
+}; 
+
+obj.a = 10;
+
+console.log(obj);
+
+console.log(name);
+var name = 'Danya';
+
+{
+    var result = 50;
+}
+
+console.log(result);
+
